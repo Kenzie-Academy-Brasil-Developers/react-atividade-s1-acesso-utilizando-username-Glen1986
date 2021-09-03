37caf8894219cf20756feacb0d458e49c279c891
glen1986 <escobar.glen@gmail.com>
2021-09-03 20:06:09 -0300

    loginComponents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import {useState} from 'react';
 import GetUserComponent from './components/GetUserComponent'
 import WelcomePage from './components/WelcomePage'
 
 function App() {
+  const user = "yo mismo"
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const botin =()=>{
+    setIsLoggedIn(
+    !isLoggedIn 
+    )
+  }
+
+    const botout =()=>{
+    console.log("logout")
+    setIsLoggedIn(
+     !isLoggedIn 
+    )
+  }
+
   return (
     <div className="App">
       <div className="App-header">
-        <GetUserComponent/>
-        <WelcomePage/>
+        <GetUserComponent
+          user = {user}
+          isLoggedIn = {isLoggedIn}
+          Logout = {botout}
+        />
+        <WelcomePage
+          user = {user}
+          Login = {botin}
+        />
      </div>
     </div>
   );
