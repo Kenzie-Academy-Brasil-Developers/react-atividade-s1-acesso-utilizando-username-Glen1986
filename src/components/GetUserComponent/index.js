28b1ff8c19be5b93fc4dde184e9d323d6482fa9e
glen1986 <escobar.glen@gmail.com>
2021-09-05 14:50:13 -0300

    2pagesok

--- src/components/GetUserComponent/index.js
+++ src/components/GetUserComponent/index.js
@@ -1,29 +1,22 @@
-import {useState} from 'react';
 
-const GetUserComponent = ({ setUser, setIsLoggedIn }) =>{
-  const[userInput, setUserInput] = useState("")
+const GetUserComponent =({setIsLoggedIn, userInput, setUserInput, userList, setUserList})=>{
 
-  const HandleLogin = ({isLoggedIn,user,setUser, userInput}) =>{
-    setIsLoggedIn(
-     isLoggedIn=true
-  )
-    setUser(
-    user = {userInput}
-  )
- }
-
-  return(
-<section>
+  const HandleLogin=()=>{
+    setUserList([userList = userInput])
+    setUserInput("")
+    setIsLoggedIn(true)
+  }
+  return (
   <form>
-   <input
-          type="text"
-          value={userInput}
-          onChange={(event) => setUserInput(event.target.value)}
-
-       /><br/>    
+    <h2>your name</h2>
+    <h3>{userInput}</h3>
+    <input value={userInput}
+           onChange={e=>setUserInput(e.target.value)} 
+             type="text" /> <br/>
+   <button href="#" onClick={(e) => HandleLogin(userInput)}>Acessar com o nome</button>
+ 
   </form>
-    <button href='#' onClick={(e) => HandleLogin(userInput)}>Acessar com o nome</button>
-</section>
-  ) 
+  )
+
 }
 export default GetUserComponent;
